27f129bcd32fce39d3fca93a79a4e3d5a012fd66
Aditya Ajay <107756503+adityaajay33@users.noreply.github.com>
2024-10-08 17:19:40 -0400

    Time: 109 ms (46.66%), Space: 17.6 MB (44.48%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,21 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         
-        distances = {len(nums)-1: 0}
-        prevGoals = set()
-        prevGoals.add(len(nums)-1)
+        res = 0
+        l = r = 0
 
-        for i in range(len(nums)-2, -1, -1):
-            minima=100000
-            for j in range(nums[i], 0, -1):
-                if (i+j)>=len(nums) or i+j not in prevGoals:
-                    continue
-                minima = min(1+distances[i+j], minima)
-            if minima==100000:
-                continue
-            prevGoals.add(i)
-            distances[i] = minima
-                
-        return distances[0]
+        while r<len(nums)-1:
+            farthest = 0
+            for i in range(l, r+1):
+                farthest = max(farthest, i+nums[i])
+
+            l=r+1
+            r = farthest
+            res += 1
+        return res
 
         
