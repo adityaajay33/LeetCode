561f178461759bf7d01d91e209dd3ec644ecf779
Aditya Ajay <107756503+adityaajay33@users.noreply.github.com>
2024-12-20 11:02:06 -0500

    Time: 122 ms (11.24%), Space: 23.9 MB (6.07%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,15 +1,13 @@
 class Solution:
     def euclidean(self, x, y):
         return (pow(x, 2) + pow(y, 2))
+    
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-
         res = []
 
         for (x, y) in points:
             output = self.euclidean(x, y)
-            ind = bisect.bisect_left(res, (output, (x, y)))
-            if ind<k:
-                res.insert(ind, (output, (x, y)))
+            heapq.heappush(res, (output, (x, y)))
 
-        return [res[i][1] for i in range(k)]
+        return [point for _, point in heapq.nsmallest(k, res)]
         
