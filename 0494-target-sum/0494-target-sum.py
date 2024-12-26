e7be59746c155686e738559a6f70b9d1ef1d5c25
Aditya Ajay <107756503+adityaajay33@users.noreply.github.com>
2024-12-26 17:19:57 -0500

    Time: 208 ms (16.69%), Space: 38.6 MB (17.29%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+
+        ''' use backtracking here to start from either the positive of the first number and the negative of teh first number. go down the backtracking list of either negative number of the second item or the positive. keep adding until the sum has been reached'''
+
+
+        dp = {}
+
+        def backtrack_nums(i, sums):
+            if (i, sums) in dp:
+                return dp[(i, sums)]
+        
+            if i==len(nums):
+                return 1 if sums==target else 0
+
+            add = backtrack_nums(i+1, sums+nums[i])
+            subtract = backtrack_nums(i+1, sums-nums[i])
+            
+            dp[(i, sums)] = add+subtract
+            return dp[(i, sums)]
+
+        return backtrack_nums(0, 0)
+
+            
+                
