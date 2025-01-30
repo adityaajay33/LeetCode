21cad3d0c24ec1b4541cc009eb451399064c745e
Aditya Ajay <107756503+adityaajay33@users.noreply.github.com>
2025-01-30 14:35:46 -0500

    Time: 15 ms (89.65%), Space: 23.5 MB (20%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,27 +1,20 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-
-        n = len(gas)
-        totals = [gas[x]-cost[x] for x in range(n)]
         
-        l, r = 0, 0
-            
-        #find the large continguous subarray of sum greater than 0
-
+        start = 0
         currSum = 0
-        while r<(2*len(gas)):
-            if (r-l)==n:
-                return l
 
-            currSum += totals[r%n]
-            r+=1
+        if sum(gas) < sum(cost):
+            return -1
+
+        for i in range(len(gas)):
+            currSum += gas[i]-cost[i]
 
             if currSum<0:
-                while l<r:
-                    currSum -= totals[l%n]
-                    l+=1
+                start=i+1
+                currSum=0
 
-        return -1
+        return start
 
 
 
